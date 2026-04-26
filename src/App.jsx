fdf33d5fb0f02504e87649f59143701ff3115521
shariful-ire <shariful.ire@gmail.com>
2026-04-26 19:18:08 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Country from './components/Countries/country';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Country></Country>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Countries/Country.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Country = () => {
+    return (
+        <div>
+            <h1>hellow, in the country </h1>
+        </div>
+    );
+};
+
+export default Country;
