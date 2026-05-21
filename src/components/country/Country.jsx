95f079c09f2a06f7292f36827f1e2581dd973651
shariful-ire <shariful.ire@gmail.com>
2026-05-21 13:54:46 +0600

    here add a button that the country visited or not and use the react hock , and the conditional rendering for the background color chagne

--- src/components/country/Country.jsx
+++ src/components/country/Country.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Country.css';
 
 const Country = ({ country }) => {
 
+    const [visited, setVisited]= useState(false);
+
+    const handleVisited=()=>{
+        setVisited(!visited)
+
+    }
+
     
 
     return (
-        <div className='country'>
+        <div className={`country ${visited? 'country-visited' : 'country-not-visited'}`}>
             <img src={country?.flags?.flags.png}></img>
             <h3>
                 Common Name: {country.name.common}
@@ -16,7 +23,11 @@ const Country = ({ country }) => {
             </h3>
             <p>Population: {country.population.population}</p>
            
-
+            <button onClick={handleVisited}>
+            {
+                visited? 'Visited': 'Not-visited'
+            }
+            </button>
         </div>
     );
 };
